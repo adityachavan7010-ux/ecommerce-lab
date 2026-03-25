c56ed954f385dc7e5c88a3aba9fb66e6ca1e2bc5
adityachavan7010-ux <adityachavan7010@gmail.com>
2026-03-25 11:54:27 +0530

    Add footer.php file for closing main tag and footer element

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+        <footer>
+            <p>&copy; 2026 E-Commerce Lab. All rights reserved. This site is developed for educational purposes.</p>
+        </footer>
+        </main>
+        <script src="assets/js/script.js"></script>
+        </body>
+        </html>
